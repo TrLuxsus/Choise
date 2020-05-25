8cd776db9fecaab63ff8258e516e890c27b5e9fb
Dima Moruga <trluxsus@outlook.com>
2020-05-25 16:01:51 +0300

    Choise. Implement independent Discipline model.

---
+++ Choise/Models/Discipline.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Choise.Models
+{
+    public class Discipline
+    {
+        public int Id { set; get; }
+        public string Title { set; get; }
+        public string Annotation { set; get; }
+    }
+}
