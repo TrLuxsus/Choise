eb356c3ed1aed0d55bdc1d5cf8bb587838e7780a
Dima Moruga <trluxsus@outlook.com>
2020-05-25 16:02:58 +0300

    Choise. Implement independent Teacher model.

---
+++ Choise/Models/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Choise.Models
+{
+    public class Teacher
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+    }
+}
