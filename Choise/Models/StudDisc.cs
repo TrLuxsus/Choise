ef51a9180848cc1720f5f5af19fda4677f507259
Dima Moruga <trluxsus@outlook.com>
2020-05-25 16:06:22 +0300

    Choise. Implement mamy to many connection between Student and Discipline in StudDisc model with navigation properties.

---
+++ Choise/Models/StudDisc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Choise.Models
+{
+    public class StudDisc
+    {
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+
+        public int DisciplineId { get; set; }
+        public Discipline Discipline { get; set; }
+    }
+}
