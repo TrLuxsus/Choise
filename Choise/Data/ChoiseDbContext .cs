3238a682ffa3ce25dcba489ae81ccd9029521cfa
Dima Moruga <trluxsus@outlook.com>
2020-05-25 16:20:42 +0300

    Choise. Create ChoiseDbContext class for db interaction.

---
+++ Choise/Data/ChoiseDbContext .cs
@@ -0,0 +1,26 @@
+﻿using Choise.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Choise.Data
+{
+    public class ChoiseDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Discipline> Disciplines { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<StudDisc> StudsDiscs { get; set; }
+
+        public ChoiseDbContext(DbContextOptions<ChoiseDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudDisc>()
+                 .HasKey(e => new { e.StudentId, e.DisciplineId });
+        }
+    }
+}
