4c34ef1104974493ffd6e0891285d2e81dcd0ab5
Dima Moruga <trluxsus@outlook.com>
2020-05-25 16:00:37 +0300

    Choise. Implement independent Student model.

---
+++ Choise/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Choise.Models
+{
+    public class Student
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Group { set; get; }
+    }
+}
