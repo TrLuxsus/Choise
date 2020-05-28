28ef78153e05bdfb3ffeac2082b34cc890b3e0b5
Dima Moruga <trluxsus@outlook.com>
2020-05-28 17:32:29 +0300

    Choise Add Students to Home page

--- Choise/Controllers/HomeController.cs
+++ Choise/Controllers/HomeController.cs
@@ -6,21 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Choise.Models;
+using Choise.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Choise.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ChoiseDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ChoiseDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Students.ToListAsync());
         }
 
         public IActionResult Privacy()
