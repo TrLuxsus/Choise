3b2bbf869d0a1cd368f138be8209991998e7a9cf
Dima Moruga <trluxsus@outlook.com>
2020-06-22 14:43:58 +0300

    Choise Teacher, Discipline server validation

--- Choise/Models/Discipline.cs
+++ Choise/Models/Discipline.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Choise.Models
     public class Discipline
     {
         public int Id { set; get; }
+        [Required]
         public string Title { set; get; }
         public string Annotation { set; get; }
 
--- Choise/Models/Teacher.cs
+++ Choise/Models/Teacher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Choise.Models
     public class Teacher
     {
         public int Id { set; get; }
+        [Required]
         public string Name { set; get; }
 
         public List<Discipline> Disciplines { set; get; }
