1c4ef19e1943f82ba32f6d84dd976d1dc186c259
Dima Moruga <trluxsus@outlook.com>
2020-06-22 14:30:07 +0300

    Choise Student server validation

--- Choise/Models/Student.cs
+++ Choise/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace Choise.Models
     public class Student
     {
         public int Id { set; get; }
+        [Required]
         public string Name { set; get; }
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
         public string Group { set; get; }
 
         public List<StudDisc> StudDiscs { set; get; }
