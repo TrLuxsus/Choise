18b40d1ba23a0d94300fb574c520cfc5207a6606
Dima Moruga <trluxsus@outlook.com>
2020-08-12 18:56:59 +0300

    Chat app

---
+++ Choise/Chat/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Chat.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chat.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Choise/Chat/Pages/Index.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Chat.Data;
+using Chat.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Chat.Pages
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        // на странице увидим протокол чата и поле ввода
+        public List<Message> Messages { set; get; }
+        [BindProperty]
+        [Required]
+        public string Text { set; get; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            Messages = _db.Messages.ToList();
+        }
+
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Messages.Add(new Message
+                {
+                    Text = Text,
+                    When = DateTime.Now,
+                    Sign = User.Identity.Name
+                });
+                _db.SaveChanges();
+            }
+            Messages = _db.Messages.ToList();
+        }
+    }
+}
